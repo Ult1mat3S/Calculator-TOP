0968337159d88dbef6c4e6eb329a051aee788535
UItimate <52617311+UItimate@users.noreply.github.com>
2022-06-30 02:07:16 -0600

    feat: Add functions to add, subtract, multiply and divide

---
+++ main.js
@@ -0,0 +1,21 @@
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+
+function subtract(num1, num2) {
+    const sum = num1 - num2;
+    return sum;
+}
+
+function multiply(num1, num2) {
+    const sum = num1 * num2;
+    return sum;
+}
+
+function divide(num1, num2) {
+    const sum = num1 / num2;
+    return sum;
+}
+
+
