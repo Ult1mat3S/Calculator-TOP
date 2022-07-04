c758cbe3e745349b6232b34b8137a16dc4dcbe35
UItimate <52617311+UItimate@users.noreply.github.com>
2022-07-04 04:20:58 -0600

    feat: have buttons display number pressed

--- main.js
+++ main.js
@@ -32,3 +32,63 @@ function operate(operator, num1, num2) {
         return divide(num1, num2);
     }
 }
+
+function button0() {
+    const display = document.querySelector('#display');
+    display.textContent = 0
+}
+
+function button1() {
+    const display = document.querySelector('#display');
+    display.textContent = 1
+}
+
+function button2() {
+    const display = document.querySelector('#display');
+    display.textContent = 2
+}
+
+function button3() {
+    const display = document.querySelector('#display');
+    display.textContent = 3
+}
+
+function button4() {
+    const display = document.querySelector('#display');
+    display.textContent = 4
+}
+
+function button5() {
+    const display = document.querySelector('#display');
+    display.textContent = 5
+}
+
+function button6() {
+    const display = document.querySelector('#display');
+    display.textContent = 6
+}
+
+function button7() {
+    const display = document.querySelector('#display');
+    display.textContent = 7
+}
+
+function button8() {
+    const display = document.querySelector('#display');
+    display.textContent = 8
+}
+
+function button9() {
+    const display = document.querySelector('#display');
+    display.textContent = 9
+}
+
+function buttonClear() {
+    const display = document.querySelector('#display');
+    display.textContent = ''
+}
+
+function buttonEqual() {
+    const display = document.querySelector('#display');
+    display.textContent = ''
+}
